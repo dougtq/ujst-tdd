64c81e56cc21d7fed242385baa2c5d1663fd1898
Douglas Alves <douglas.expalves@gmail.com>
2017-11-03 14:42:53 -0200

    update calculator lib to es6

--- src/calc/index.js
+++ src/calc/index.js
@@ -1,28 +1,31 @@
-module.exports = {
-  sum (...val) {
-    let result = 0
-    val.forEach(num => {
-      result += Number(num)
-    })
-    return result
-  },
-  sub (...val) {
-    let result = val.splice(0, 1)
-    val.forEach(num => {
-      result = result - Number(num)
-    })
-    return result
-  },
-  mult (...val) {
-    let result = val.splice(0, 1)
-    val.forEach(num => {
-      result = result * Number(num)
-    })
-    return result
-  },
-  div (val1, val2) {
-    return Number(val2) === 0
-      ? 'Não é possível realizar divisões por 0'
-      : Number(val1) / Number(val2)
-  }
+let sum = (...val) => {
+  let result = 0
+  val.forEach(num => {
+    result += Number(num)
+  })
+  return result
 }
+
+let sub = (...val) => {
+  let result = val.splice(0, 1)
+  val.forEach(num => {
+    result = result - Number(num)
+  })
+  return result
+}
+
+let mult = (...val) => {
+  let result = val.splice(0, 1)
+  val.forEach(num => {
+    result = result * Number(num)
+  })
+  return result
+}
+
+let div = (val1, val2) => {
+  return Number(val2) === 0
+    ? 'Não é possível realizar divisões por 0'
+    : Number(val1) / Number(val2)
+}
+
+export { sum, sub, mult, div }
