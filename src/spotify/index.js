b2a4bf4e9ca202ce1d2f479adde88c75a35e7283
Douglas Alves <douglas.expalves@gmail.com>
2017-11-06 23:36:06 -0200

    full spotify query and tests

--- src/spotify/index.js
+++ src/spotify/index.js
@@ -1,10 +1,14 @@
 let search = (q, type) =>
   fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`)
-    .then(data => {
-      return data
-    })
-    .catch(err => {
-      console.log(err)
-    })
+    .then(data => data)
+    .catch(err => err.message)
 
-export { search }
+let searchArtist = q => search(q, 'artist')
+
+let searchAlbum = q => search(q, 'album')
+
+let searchTrack = q => search(q, 'track')
+
+let searchPlaylist = (q) => search(q, 'playlist')
+
+export { search, searchArtist, searchAlbum, searchPlaylist, searchTrack }
--- tests/spotify.spec.js
+++ tests/spotify.spec.js
@@ -4,10 +4,10 @@ import sinonChai from 'sinon-chai'
 import sinonStubPromise from 'sinon-stub-promise'
 import {
   search,
-  searchAlbums,
+  searchAlbum,
   searchArtist,
   searchPlaylist,
-  searchTracks
+  searchTrack
 } from '../src/spotify/'
 chai.use(sinonChai)
 sinonStubPromise(sinon)
@@ -15,6 +15,16 @@ sinonStubPromise(sinon)
 global.fetch = require('node-fetch')
 
 describe('Spotify Wrapper', () => {
+  let fetchedStub, promise, data
+
+  beforeEach('Lidando com Stubs', () => {
+    fetchedStub = sinon.stub(global, 'fetch')
+    promise = fetchedStub.returnsPromise()
+  })
+
+  afterEach('Restaurando Stubs', () => {
+    fetchedStub.restore()
+  })
   describe('Smoke Tests', () => {
     it('should exist the search method', () => {
       expect(search).to.exist
@@ -23,10 +33,10 @@ describe('Spotify Wrapper', () => {
       expect(searchArtist).to.exist
     })
     it('should exist the searchTracks method', () => {
-      expect(searchTracks).to.exist
+      expect(searchTrack).to.exist
     })
     it('should exist the searchAlbums method', () => {
-      expect(searchAlbums).to.exist
+      expect(searchAlbum).to.exist
     })
     it('should exist the searchPlaylists method', () => {
       expect(searchPlaylist).to.exist
@@ -34,17 +44,6 @@ describe('Spotify Wrapper', () => {
   })
 
   describe('General Search', () => {
-    let fetchedStub, promise, data
-
-    beforeEach('Lidando com Stubs', () => {
-      fetchedStub = sinon.stub(global, 'fetch')
-      promise = fetchedStub.returnsPromise()
-    })
-
-    afterEach('Restaurando Stubs', () => {
-      fetchedStub.restore()
-    })
-
     it('should call fetch function', () => {
       search()
       expect(fetchedStub).to.been.calledOnce
@@ -71,6 +70,62 @@ describe('Spotify Wrapper', () => {
       promise.resolves({ body: 'json' })
       data = search('kanye', 'artist')
       expect(data.resolveValue).to.eql({ body: 'json' })
+    })
+  })
+
+  describe('Search Artist', () => {
+    it('should call fetch funtion', () => {
+      searchArtist('Kanye')
+      expect(fetchedStub).to.been.calledOnce
+    })
+
+    it('should call fetch funtion with the correct url', () => {
+      searchArtist('Kanye')
+      expect(fetchedStub).to.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Kanye&type=artist'
+      )
+    })
+  })
+
+  describe('Search Album', () => {
+    it('should call fetch funtion', () => {
+      searchAlbum('Yeezus')
+      expect(fetchedStub).to.been.calledOnce
+    })
+
+    it('should call fetch funtion with the correct url', () => {
+      searchAlbum('Yeezus')
+      expect(fetchedStub).to.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Yeezus&type=album'
+      )
+    })
+  })
+
+  describe('Search Track', () => {
+    it('should call fetch funtion', () => {
+      searchTrack('Karaoke')
+      expect(fetchedStub).to.been.calledOnce
+    })
+
+    it('should call fetch funtion with the correct url', () => {
+      searchTrack('Karaoke')
+      expect(fetchedStub).to.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Karaoke&type=track'
+      )
+    })
+  })
+
+  describe('Search Playlist', () => {
+    it('should call fetch funtion', () => {
+      searchPlaylist('Drake')
+      expect(fetchedStub).to.been.calledOnce
+    })
+
+    it('should call fetch funtion with the correct url', () => {
+      searchPlaylist('Drake')
+      expect(fetchedStub).to.been.calledWith(
+        'https://api.spotify.com/v1/search?q=Drake&type=playlist'
+      )
     })
   })
 })
