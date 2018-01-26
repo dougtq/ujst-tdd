d5f20f291199a800d23951ee4ab6aaf1b1134b1d
Douglas Alves <douglas.expalves@gmail.com>
2018-01-26 12:48:24 -0200

    update fetch var to global.fetch

--- src/spotify/index.js
+++ src/spotify/index.js
@@ -1,14 +1,16 @@
-let search = (q, type) =>
-  fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`)
+// const fetch = require('node-fetch')
+
+const search = (q, type) =>
+  global.fetch(`https://api.spotify.com/v1/search?q=${q}&type=${type}`)
     .then(data => data)
     .catch(err => err.message)
 
-let searchArtist = q => search(q, 'artist')
+const searchArtist = q => search(q, 'artist')
 
-let searchAlbum = q => search(q, 'album')
+const searchAlbum = q => search(q, 'album')
 
-let searchTrack = q => search(q, 'track')
+const searchTrack = q => search(q, 'track')
 
-let searchPlaylist = (q) => search(q, 'playlist')
+const searchPlaylist = (q) => search(q, 'playlist')
 
 export { search, searchArtist, searchAlbum, searchPlaylist, searchTrack }
